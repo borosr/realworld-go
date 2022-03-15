aa9eecac995c0513684609d2aaf986e3c501b611
Robert Boros <robbie13.boros@gmail.com>
2022-03-15 22:16:42 +0100

    move user logic to domain

--- api/users.go
+++ api/users.go
@@ -4,18 +4,15 @@ import (
 	"context"
 	goTypes "go/types"
 	"net/http"
-	"time"
 
+	"github.com/borosr/realworld/domain"
 	"github.com/borosr/realworld/lib/api"
-	"github.com/borosr/realworld/lib/auth"
 	"github.com/borosr/realworld/lib/middleware"
-	"github.com/borosr/realworld/persist"
 	"github.com/borosr/realworld/types"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type userController struct {
-	userRepository persist.Repository[*types.User]
+	userService domain.UserDescriptor
 }
 
 func (uc userController) Init() {
@@ -48,58 +45,31 @@ func (uc userController) Init() {
 
 func (uc userController) login(ctx context.Context, u types.UserWrapper[types.UserLogin]) (types.UserWrapper[types.User], error) {
 	var fallback types.UserWrapper[types.User]
-	user, err := uc.userRepository.Get(ctx, u.User.Email)
+	user, err := uc.userService.Login(ctx, u.User)
 	if err != nil {
 		return fallback, err
 	}
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(u.User.Password)); err != nil {
-		return fallback, err
-	}
-	if user.Token == "" {
-		token, err := auth.Sign(map[string]interface{}{
-			"email": user.Email,
-			"iat":   time.Now().UTC().Unix(),
-		})
-		if err != nil {
-			return fallback, err
-		}
-		user.Token = token
-		if _, err := uc.userRepository.Save(ctx, user); err != nil {
-			return fallback, err
-		}
-	}
-
-	fallback.User = *user
+	fallback.User = user
 	return fallback, nil
 }
 
 func (uc userController) registration(ctx context.Context, u types.UserWrapper[types.UserSignUp]) (types.UserWrapper[types.User], error) {
 	var fallback types.UserWrapper[types.User]
-	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(u.User.Password), bcrypt.DefaultCost)
+	user, err := uc.userService.SignUp(ctx, u.User)
 	if err != nil {
 		return fallback, err
 	}
-	saved, err := uc.userRepository.Save(ctx, &types.User{
-		Email: u.User.Email,
-		Profile: types.Profile{
-			Username: u.User.Username,
-		},
-		Password: string(encryptedPassword),
-	})
-	if err != nil {
-		return fallback, err
-	}
-	fallback.User = *saved
+	fallback.User = user
 	return fallback, nil
 }
 
-func (uc userController) currentUser(ctx context.Context, u goTypes.Nil) (types.UserWrapper[types.User], error) {
+func (uc userController) currentUser(ctx context.Context, _ goTypes.Nil) (types.UserWrapper[types.User], error) {
 	var fallback types.UserWrapper[types.User]
-	username, err := api.GetValue[string](ctx, "email")
+	email, err := api.GetValue[string](ctx, "email")
 	if err != nil {
 		return fallback, err
 	}
-	user, err := uc.getAuthenticatedUser(ctx, username)
+	user, err := uc.userService.GetByEmail(ctx, email)
 	if err != nil {
 		return fallback, err
 	}
@@ -109,40 +79,10 @@ func (uc userController) currentUser(ctx context.Context, u goTypes.Nil) (types.
 
 func (uc userController) updateUser(ctx context.Context, u types.UserWrapper[types.User]) (types.UserWrapper[types.User], error) {
 	var fallback types.UserWrapper[types.User]
-	user, err := uc.userRepository.Get(ctx, u.User.Email)
+	user, err := uc.userService.Update(ctx, u.User)
 	if err != nil {
 		return fallback, err
 	}
-	if u.User.Email != "" {
-		user.Email = u.User.Email
-	}
-	if u.User.Token != "" {
-		user.Token = u.User.Token
-	}
-	if u.User.Username != "" {
-		user.Username = u.User.Username
-	}
-	if u.User.Bio != "" {
-		user.Bio = u.User.Bio
-	}
-	if u.User.Image != "" {
-		user.Image = u.User.Image
-	}
-	if u.User.Password != "" {
-		user.Password = u.User.Password
-	}
-	saved, err := uc.userRepository.Save(ctx, user)
-	if err != nil {
-		return fallback, err
-	}
-	fallback.User = *saved
+	fallback.User = user
 	return fallback, nil
-}
-
-func (uc userController) getAuthenticatedUser(ctx context.Context, username string) (types.User, error) {
-	user, err := uc.userRepository.Get(ctx, username)
-	if err != nil {
-		return types.User{}, err
-	}
-	return *user, nil
 }
---
+++ domain/users.go
@@ -0,0 +1,102 @@
+package domain
+
+import (
+	"context"
+	"time"
+
+	"github.com/borosr/realworld/lib/auth"
+	"github.com/borosr/realworld/persist"
+	"github.com/borosr/realworld/types"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserDescriptor interface {
+	Login(ctx context.Context, u types.UserLogin) (types.User, error)
+	SignUp(ctx context.Context, u types.UserSignUp) (types.User, error)
+	GetByEmail(ctx context.Context, email string) (types.User, error)
+	Update(ctx context.Context, u types.User) (types.User, error)
+}
+
+type UserService struct {
+	userRepository persist.Repository[*types.User]
+}
+
+func (us UserService) Login(ctx context.Context, u types.UserLogin) (types.User, error) {
+	user, err := us.userRepository.Get(ctx, u.Email)
+	if err != nil {
+		return types.User{}, err
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(u.Password)); err != nil {
+		return types.User{}, err
+	}
+	if user.Token == "" {
+		token, err := auth.Sign(map[string]interface{}{
+			"email": user.Email,
+			"iat":   time.Now().UTC().Unix(),
+		})
+		if err != nil {
+			return types.User{}, err
+		}
+		user.Token = token
+		if _, err := us.userRepository.Save(ctx, user); err != nil {
+			return types.User{}, err
+		}
+	}
+	return *user, nil
+}
+
+func (us UserService) SignUp(ctx context.Context, u types.UserSignUp) (types.User, error) {
+	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return types.User{}, err
+	}
+	saved, err := us.userRepository.Save(ctx, &types.User{
+		Email: u.Email,
+		Profile: types.Profile{
+			Username: u.Username,
+		},
+		Password: string(encryptedPassword),
+	})
+	if err != nil {
+		return types.User{}, err
+	}
+	return *saved, nil
+}
+
+func (us UserService) GetByEmail(ctx context.Context, email string) (types.User, error) {
+	user, err := us.userRepository.Get(ctx, email)
+	if err != nil {
+		return types.User{}, err
+	}
+	return *user, nil
+}
+
+func (us UserService) Update(ctx context.Context, u types.User) (types.User, error) {
+	user, err := us.userRepository.Get(ctx, u.Email)
+	if err != nil {
+		return types.User{}, err
+	}
+	if u.Email != "" {
+		user.Email = u.Email
+	}
+	if u.Token != "" {
+		user.Token = u.Token
+	}
+	if u.Username != "" {
+		user.Username = u.Username
+	}
+	if u.Bio != "" {
+		user.Bio = u.Bio
+	}
+	if u.Image != "" {
+		user.Image = u.Image
+	}
+	if u.Password != "" {
+		user.Password = u.Password
+	}
+	saved, err := us.userRepository.Save(ctx, user)
+	if err != nil {
+		return types.User{}, err
+	}
+	return *saved, nil
+}
